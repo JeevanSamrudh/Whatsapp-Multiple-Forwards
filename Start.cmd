81920127827a484e80595bcf49232cbc1af8a901
JeevanSamrudh <61643582+JeevanSamrudh@users.noreply.github.com>
2021-04-12 18:29:15 +0530

    Update Start.cmd

--- Start.cmd
+++ Start.cmd
@@ -1,7 +1,7 @@
 echo on
 cls
 title Opening the Required Files
-cd "C:\Users\91807\Desktop\Project\DOTD\New"
+cd "C:\current-working-directory"
 start "" chrome.exe --remote-debugging-port=9014 --user-data-dir="C:\Users\91807\Desktop\Project\DOTD\New\Chrome_Profile" "https://web.whatsapp.com/"
 pause
 py -3 prog1.py
